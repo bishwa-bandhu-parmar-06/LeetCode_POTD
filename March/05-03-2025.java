6f53fa503778b460c02054cdfcf7f3996dd65e6c
bishwabandhu <bishwabandhuparmar06@gmail.com>
2025-03-05 19:05:38 +0530

    2579. Count Total Number of Colored Cells

---
+++ March/05-03-2025.java
@@ -0,0 +1,8 @@
+
+// 2579. Count Total Number of Colored Cells
+class Solution {
+    public long coloredCells(int n) {
+        if(n == 1) return 1;
+        return 2L * n * (n-1) + 1;  // L -> To prevent overflow
+    }
+}
