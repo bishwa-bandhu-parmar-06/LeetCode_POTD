f2d0ad3e6bdd2258aaef5b4ca188a581bc52d163
bishwabandhu <bishwabandhuparmar06@gmail.com>
2025-03-17 21:35:30 +0530

    2206. Divide Array Into Equal Pairs

---
+++ March/17-03-25.java
@@ -0,0 +1,18 @@
+// 2206. Divide Array Into Equal Pairs
+
+class Solution {
+    public boolean divideArray(int[] nums) {
+        Map<Integer, Integer> counter = new HashMap<>();
+        for (int num : nums) {
+            counter.put(num, counter.getOrDefault(num, 0) + 1);
+        }
+        
+        for (int count : counter.values()) {
+            if (count % 2 != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
