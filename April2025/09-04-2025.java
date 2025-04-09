a54c70a108562c6b150ecd42a3f41f030cfe1eb3
bishwabandhu <bishwabandhuparmar06@gmail.com>
2025-04-09 23:01:07 +0530

    3375. Minimum Operations to Make Array Values Equal to K

---
+++ April2025/09-04-2025.java
@@ -0,0 +1,29 @@
+// 3375. Minimum Operations to Make Array Values Equal to K
+
+import java.util.*;
+
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int countK = 0, greater = 0;
+        int lastGreater = -1;
+        boolean smallerExist = false;
+
+        for (int num : nums) {
+            if (num == k) {
+                countK++;
+            } else if (num > k) {
+                if (lastGreater != num) {
+                    lastGreater = num;
+                    greater++;
+                }
+            } else {
+                smallerExist = true;
+            }
+        }
+
+        if (countK == nums.length) return 0;
+        if (smallerExist) return -1;
+        return greater;
+    }
+}
