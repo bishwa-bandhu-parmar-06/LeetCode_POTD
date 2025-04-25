2a3ecaf18961177b3735fe331df09d77edfc6ac1
bishwabandhu <bishwabandhuparmar06@gmail.com>
2025-04-25 06:11:01 +0530

    2845. Count of Interesting Subarrays

---
+++ April2025/25-04-2025.java
@@ -0,0 +1,24 @@
+// 2845. Count of Interesting Subarrays
+
+class Solution {
+    public long countInterestingSubarrays(List<Integer> nums, int modulo, int k) {
+          Map<Integer, Long> freq = new HashMap<>();
+        freq.put(0, 1L); // base case for prefix
+        long count = 0;
+        int prefix = 0;
+
+        for (int num : nums) {
+            if (num % modulo == k) {
+                prefix++;
+            }
+
+            int currentMod = prefix % modulo;
+            int targetMod = (currentMod - k + modulo) % modulo;
+
+            count += freq.getOrDefault(targetMod, 0L);
+            freq.put(currentMod, freq.getOrDefault(currentMod, 0L) + 1);
+        }
+
+        return count;
+    }
+}
