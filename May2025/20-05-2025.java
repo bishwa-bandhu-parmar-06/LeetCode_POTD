d28afbefc9801fb651d965316f377963127f826f
bishwabandhu <bishwabandhuparmar06@gmail.com>
2025-05-20 23:34:54 +0530

    3355. Zero Array Transformation I

--- May2025/20-05-2025.java
+++ May2025/20-05-2025.java
@@ -2,19 +2,26 @@
 
 class Solution {
     public boolean isZeroArray(int[] nums, int[][] queries) {
-        for (int i = 0; i < queries.length; i++) {
-            for (int j = queries[i][0]; j <= queries[i][1]; j++) {
-                if (nums[j] > 0) {
-                    nums[j]--;
-                }
+        int n = nums.length;
+        int[] diff = new int[n + 1];
+
+        for (int[] q : queries) {
+            int l = q[0];
+            int r = q[1];
+            diff[l]++;
+            if (r + 1 < n) {
+                diff[r + 1]--;
             }
         }
 
-        for (int x : nums) {
-            if (x != 0) {
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            cnt += diff[i];
+            if (nums[i] > cnt) {
                 return false;
             }
         }
+
         return true;
     }
 }
