e6b5be42ea98c11cdfb3e1f5194e514c265a77e7
bishwabandhu <bishwabandhuparmar06@gmail.com>
2025-05-24 16:49:10 +0530

    2942. Find Words Containing Character

---
+++ May2025/24-05-2025.java
@@ -0,0 +1,15 @@
+// 2942. Find Words Containing Character
+
+import java.util.*;
+
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].indexOf(x) != -1) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
