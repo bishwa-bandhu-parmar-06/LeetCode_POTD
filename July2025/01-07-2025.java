5903ad92f3ce6cf50934777c0231eed9b5561a88
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2025-07-01 07:53:47 +0530

    3330. Find the Original Typed String I

---
+++ July2025/01-07-2025.java
@@ -0,0 +1,23 @@
+// 3330. Find the Original Typed String I
+
+
+
+
+class Solution {
+    public int possibleStringCount(String word) {
+        int res = 1;
+        int i = 0, l = word.length(), val;
+        char cur;
+        while (i < l) {
+            val = 1;
+            cur = word.charAt(i);
+            while (i < l - 1 && cur == word.charAt(i + 1)) {
+                val++;
+                i++;
+            }
+            i++;
+            res += val - 1;
+        }
+        return res;
+    }
+}
