3800de079ab7d32c241e524dd70dbed3c4b31a57
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2025-09-12 21:26:59 +0530

    3227. Vowels Game in a String

---
+++ September2025/12-09-2025.java
@@ -0,0 +1,11 @@
+// 3227. Vowels Game in a String
+
+
+class Solution {
+    public boolean doesAliceWin(String s) {
+        for (int i = 0; i < s.length(); i++)
+            if ((0x104111 >> (s.charAt(i) - 97) & 1) != 0)
+                return true;
+        return false;
+    }
+}
