1a58eb13c3b02d37ba646a5828e1e991ef217296
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2025-09-15 21:08:39 +0530

    1935. Maximum Number of Words You Can Type

---
+++ September2025/15-09-2025.java
@@ -0,0 +1,20 @@
+// 1935. Maximum Number of Words You Can Type
+
+
+class Solution {
+    public int canBeTypedWords(String text, String brokenLetters) {
+        String[] words = text.split(" ");
+        int count = 0;
+        for (String word : words) {
+            boolean canType = true;
+            for (char c : brokenLetters.toCharArray()) {
+                if (word.indexOf(c) != -1) {
+                    canType = false;
+                    break;
+                }
+            }
+            if (canType) count++;
+        }
+        return count;
+    }
+}
