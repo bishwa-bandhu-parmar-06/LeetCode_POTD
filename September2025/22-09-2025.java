862ab4ddff6ad443d4427c7d52d9016ed2d2cd68
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2025-09-22 14:09:40 +0530

    3005. Count Elements With Maximum Frequency

---
+++ September2025/22-09-2025.java
@@ -0,0 +1,16 @@
+// 3005. Count Elements With Maximum Frequency
+
+class Solution {
+    public int maxFrequencyElements(int[] nums) {
+        byte[] freq = new byte[101];
+        byte max = 0, res = 0;
+        for (int n : nums) {
+            byte f = ++freq[n];
+            if (f > max) {
+                max = f; res = f;
+            } else if (f == max) 
+                res += f;
+        }
+        return res;
+    }
+}
