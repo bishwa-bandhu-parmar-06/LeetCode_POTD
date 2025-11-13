7e77a4d02828a4068e67ee52219de0e0240fdd61
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2025-11-13 19:50:49 +0530

    3228. Maximum Number of Operations to Move Ones to the End

---
+++ November_2025/13-11-2025.java
@@ -0,0 +1,17 @@
+// 3228. Maximum Number of Operations to Move Ones to the End
+
+
+
+
+class Solution {
+    static public int maxOperations(String s) {
+        int cnt = 0, n = s.length(), cnt1 = (s.charAt(0) - '0');
+        for (int i = 1; i < n; i++){
+            int x = s.charAt(i) - '0';
+            cnt1 += x;
+            cnt += (x == 0 && s.charAt(i-1) - '0' == 1) ? cnt1 : 0;
+        }
+        
+        return cnt;
+    }
+}
