8a9bc1a2eebd444f4c80dcf9ab86fb21651071e7
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2025-11-17 19:05:06 +0530

    1437. Check If All 1's Are at Least Length K Places Away

---
+++ November_2025/17-11-2025.java
@@ -0,0 +1,17 @@
+// 1437. Check If All 1's Are at Least Length K Places Away
+
+
+class Solution {
+    public boolean kLengthApart(int[] nums, int k) {
+        final int n=nums.length;
+        int last=-100000000;
+        for(int i=0; i<n; i++){
+            if (nums[i]==1){
+                if (i-last-1<k) return false;
+                last=i;
+            }
+        }
+        return true;
+        
+    }
+}
