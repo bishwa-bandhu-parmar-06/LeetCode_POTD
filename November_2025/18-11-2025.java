e5a5fd3f9f187f126c8de825e84b85ab919796e7
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2025-11-18 22:52:31 +0530

    717. 1-bit and 2-bit Characters

---
+++ November_2025/18-11-2025.java
@@ -0,0 +1,13 @@
+
+
+// 717. 1-bit and 2-bit Characters
+
+class Solution {
+    public boolean isOneBitCharacter(int[] bits) {
+        for(int i = 0; i < bits.length; i++) {
+            if(i == bits.length - 1) return true;
+            if(bits[i] == 1) i++;
+        }
+        return false;
+    }
+}
