71faaa4e9d4f61641a532946080e9b0d73a2ea77
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2025-11-19 19:35:31 +0530

    2154. Keep Multiplying Found Values by Two

---
+++ November_2025/19-11-2025.java
@@ -0,0 +1,17 @@
+// 2154. Keep Multiplying Found Values by Two
+
+
+
+class Solution {
+    public int findFinalValue(int[] nums, int k) {
+        int bits = 0;
+        for (int num : nums) {
+            if (num % k != 0) continue;
+            int n = num / k;
+            if ((n & (n - 1)) == 0)
+                bits |= n;
+        }
+        bits++;
+        return k * (bits & -bits);
+    }
+}
