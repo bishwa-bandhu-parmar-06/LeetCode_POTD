55e6f3e0f4142fd918a99c6688f85ef92687e854
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2025-11-22 19:46:28 +0530

    3190. Find Minimum Operations to Make All Elements Divisible by Three

---
+++ November_2025/22-11-2025.java
@@ -0,0 +1,15 @@
+
+// 3190. Find Minimum Operations to Make All Elements Divisible by Three
+
+
+
+
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int res = 0;
+        for (int num : nums) {
+            if (num % 3 != 0) res++;
+        }
+        return res;
+    }
+}
