ae208fd37c84c174dc6157c6ef4cc519714170ef
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2025-12-26 10:25:00 +0530

    2483. Minimum Penalty for a Shop

---
+++ December-2025/26-12-2025.java
@@ -0,0 +1,21 @@
+// 2483. Minimum Penalty for a Shop
+
+
+
+class Solution {
+    public int bestClosingTime(String customers) {
+        int penalty = 0, best_penalty = 0, ans = 0;
+        char[] ch = customers.toCharArray();
+        for(int i = 0; i < customers.length(); i++) {
+            if(ch[i] == 'Y') {
+                penalty--;
+                if(penalty < best_penalty) {
+                    best_penalty = penalty;
+                    ans = i + 1;
+                }
+            }
+            else penalty++;
+        }
+        return ans;
+    }
+}
