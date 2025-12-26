945a74f1926398dc92b822cf1de82d7c15702cba
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2025-12-26 10:23:51 +0530

    3075. Maximize Happiness of Selected Children

---
+++ December-2025/25-12-2025.java
@@ -0,0 +1,18 @@
+// 3075. Maximize Happiness of Selected Children
+
+
+class Solution {
+    public long maximumHappinessSum(int[] happiness, int k) {
+        Arrays.sort(happiness);
+        int count = 0;
+        long res = 0;
+        for(int i=happiness.length-1;i>=happiness.length-k;i--){
+            if(happiness[i] + count > 0){
+                res+=(long)(happiness[i] + count);
+            }
+            count--;
+        }
+
+        return res;
+    }
+}
