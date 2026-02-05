bd51a4dd9145b49f582eaec3dceaa71861502477
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2026-02-05 10:10:54 +0530

    3379. Transformed Array

---
+++ February-2026/05-01-2026.java
@@ -0,0 +1,17 @@
+// 3379. Transformed Array
+
+
+
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int targetIdx = ((i + nums[i]) % n + n) % n;
+            result[i] = nums[targetIdx];
+        }
+
+        return result;
+    }
+}
