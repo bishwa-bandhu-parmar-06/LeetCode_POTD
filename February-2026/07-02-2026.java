2e0db2fc3a2dbda3cd3594d418dd6add5d3ea942
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2026-02-07 11:27:47 +0530

    1653. Minimum Deletions to Make String Balanced

---
+++ February-2026/07-02-2026.java
@@ -0,0 +1,17 @@
+
+// 1653. Minimum Deletions to Make String Balanced
+
+
+class Solution {
+    public int minimumDeletions(String s) {
+        int b_before_a = 0, deletion = 0;
+        for(char ch : s.toCharArray()) {
+            if(ch == 'b') b_before_a += 1;
+            else if(b_before_a > 0 ) {
+                b_before_a -= 1;
+                deletion += 1;
+            }
+        }   
+        return deletion;
+    }
+}
