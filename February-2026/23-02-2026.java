0bd6270049132ab997522444488dce6526ac9d4f
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2026-02-23 09:45:52 +0530

    1461. Check If a String Contains All Binary Codes of Size K

---
+++ February-2026/23-02-2026.java
@@ -0,0 +1,26 @@
+// 1461. Check If a String Contains All Binary Codes of Size K
+
+
+
+
+class Solution {
+    public boolean hasAllCodes(String s, int k) {
+        //int total = (int)Math.pow(2,k);
+        int total = 1<<k; 
+        Set<String> set=new HashSet();
+        
+
+        for(int i=k; i<=s.length(); i++){
+            String s1=s.substring(i-k,i);
+
+            if(!set.contains(s1)){
+                set.add(s1);
+            }        
+        }
+
+        if(set.size()==total)  return true;
+
+
+        return false;
+    }
+}
