4c45d4845d97aed4b1d2ff488b95bbbf25d10f92
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2026-02-28 10:59:51 +0530

    1680. Concatenation of Consecutive Binary Numbers

---
+++ February-2026/28-02-2026.java
@@ -0,0 +1,18 @@
+// 1680. Concatenation of Consecutive Binary Numbers
+
+
+class Solution {
+    public int concatenatedBinary(int n) {
+        int MOD = (int)1e9 + 7;
+        long ans = 0;
+        int bitLen = 0;
+
+        for(int i = 1; i <= n; i++){
+            if((i & (i-1)) == 0) bitLen++;
+
+            ans = ((ans << bitLen)+i) % MOD;
+        }
+        
+        return (int)ans;
+    }
+}
