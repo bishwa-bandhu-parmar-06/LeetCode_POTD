c058668e6fbc8f2a4ba4018f8f4b0621e79b5e0d
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2026-04-13 09:16:41 +0530

    1848. Minimum Distance to the Target Element

---
+++ April-2026/13-04-2026.java
@@ -0,0 +1,19 @@
+
+// 1848. Minimum Distance to the Target Element
+
+
+class Solution {
+    public int getMinDistance(int[] nums, int target, int start) {
+
+        if(nums[start] == target) return 0;
+
+        int n = nums.length;
+        int d = 1;
+
+        while(true){
+            if(start - d >= 0 && nums[start - d] == target) return d;
+            if(start + d < n && nums[start + d] == target) return d;
+            d++;
+        }
+    }
+}
