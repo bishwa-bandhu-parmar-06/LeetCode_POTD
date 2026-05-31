f3150d2486649a8daadccf5d01f99e9ef4e093eb
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2026-05-31 08:08:18 +0530

    2126. Destroying Asteroids

---
+++ May-2026/31-05-2026.java
@@ -0,0 +1,20 @@
+
+
+// 2126. Destroying Asteroids
+
+import java.util.Arrays;
+
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+
+        long cur = mass;
+
+        for (int a : asteroids) {
+            if (cur < a) return false;
+            cur += a;
+        }
+
+        return true;
+    }
+}
