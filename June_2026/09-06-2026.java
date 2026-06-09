ff90bdaa11943610730ce48e2871ba2121233111
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2026-06-09 06:00:48 +0530

    3689. Maximum Total Subarray Value I

---
+++ June_2026/09-06-2026.java
@@ -0,0 +1,14 @@
+
+// 3689. Maximum Total Subarray Value I
+
+
+class Solution {
+    public long maxTotalValue(int[] num, int k) {
+        int mxv = Integer.MIN_VALUE, mnv = Integer.MAX_VALUE;
+        for (int val : num) {
+            mxv = Math.max(mxv, val);
+            mnv = Math.min(mnv, val);
+        }
+        return 1L * (mxv - mnv) * k;
+    }
+}
