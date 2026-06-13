e9ee96a3eb357e9deb4ede7c8142e89b463e0729
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2026-06-13 09:03:25 +0530

    3838. Weighted Word Mapping

---
+++ June_2026/13-06-2026.java
@@ -0,0 +1,24 @@
+
+
+
+
+// 3838. Weighted Word Mapping
+
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+        StringBuilder ans = new StringBuilder();
+
+        for (String word : words) {
+            long sum = 0;
+
+            for (char ch : word.toCharArray()) {
+                sum += weights[ch - 'a'];
+            }
+
+            int remainder = (int)(sum % 26);
+            ans.append((char)('z' - remainder));
+        }
+
+        return ans.toString();
+    }
+}
