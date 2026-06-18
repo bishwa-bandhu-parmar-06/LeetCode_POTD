bf02dccd76d148cab3c49f722245bb2b48549678
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2026-06-18 08:39:46 +0530

    1344. Angle Between Hands of a Clock

---
+++ June_2026/18-06-2026.py
@@ -0,0 +1,10 @@
+
+# 1344. Angle Between Hands of a Clock
+
+
+
+class Solution:
+    def angleClock(self, hour: int, minutes: int) -> float:
+        x = hour + minutes / 60
+        diff = (11 * x) % 12
+        return min(diff, 12 - diff) * 30
