98fe38f7e35009079e89a00df42dbe9bb4b750b9
Bishwa Bandhu Parmar <bishwabandhuparmar06@gmail.com>
2026-06-25 23:26:58 +0530

    3737. Count Subarrays With Majority Element I

---
+++ June_2026/25-06-2026.py
@@ -0,0 +1,23 @@
+
+# 3737. Count Subarrays With Majority Element I
+
+
+from typing import List
+
+class Solution:
+    def countMajoritySubarrays(self, nums: List[int], target: int) -> int:
+        op = 0
+
+        for i in range(len(nums)):
+            cnt = 0
+
+            for j in range(i, len(nums)):
+                if nums[j] == target:
+                    cnt += 1
+                else:
+                    cnt -= 1
+
+                if cnt > 0:
+                    op += 1
+
+        return op
