e2344982465f45149c4553b460641d8a0882c433
bishwa-bandhu-parmar-06 <bishwabandhuparmar06@gmail.com>
2026-07-26 22:54:40 +0530

    628. Maximum Product of Three Numbers

---
+++ July_2026/26-07-2026.py
@@ -0,0 +1,8 @@
+
+# 628. Maximum Product of Three Numbers
+
+
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        (a, b, c), (x, y) = nlargest(3, nums), nsmallest(2, nums)
+        return max(a * b * c, a * x * y)
