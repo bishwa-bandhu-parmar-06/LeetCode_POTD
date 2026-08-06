5c300d0b8d5b377f3c2245048f17c89f29344cac
bishwa-bandhu-parmar-06 <bishwabandhuparmar06@gmail.com>
2026-08-06 07:28:48 +0530

    3345. Smallest Divisible Digit Product I

---
+++ August_2026/06-08-2026.py
@@ -0,0 +1,12 @@
+
+# 3345. Smallest Divisible Digit Product I
+
+class Solution:
+    def smallestNumber(self, n: int, t: int) -> int:
+        while True:
+            prod = 1
+            for d in str(n):
+                prod *= int(d)
+            if prod % t == 0:
+                return n
+            n += 1
