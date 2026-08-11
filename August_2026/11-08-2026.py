5ac679f923677711f07d54b63c640e1bf28c7d98
bishwa-bandhu-parmar-06 <bishwabandhuparmar06@gmail.com>
2026-08-11 07:20:01 +0530

    2996. Smallest Missing Integer Greater Than Sequential Prefix Sum

---
+++ August_2026/11-08-2026.py
@@ -0,0 +1,20 @@
+
+# 2996. Smallest Missing Integer Greater Than Sequential Prefix Sum
+
+
+class Solution:
+    def missingInteger(self, nums: list[int]) -> int:
+        prefix_len = 1
+        num_set = set(nums)
+
+        for prev, curr in zip(nums, nums[1:]):
+            if curr == prev + 1:
+                prefix_len += 1
+            else:
+                break
+
+        total = (nums[prefix_len - 1] + nums[0]) * prefix_len // 2
+        while total in num_set:
+            total += 1
+
+        return total
