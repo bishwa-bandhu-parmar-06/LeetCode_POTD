3cc2ce170dcbaf313c4971c71fd36ba3bc1c357c
bishwa-bandhu-parmar-06 <bishwabandhuparmar06@gmail.com>
2026-08-15 22:13:26 +0530

    3702. Longest Subsequence With Non-Zero Bitwise XOR

---
+++ August_2026/15-08-2026.py
@@ -0,0 +1,6 @@
+
+# 3702. Longest Subsequence With Non-Zero Bitwise XOR
+
+class Solution:
+    def longestSubsequence(self, nums: List[int]) -> int:
+        return 0 if all(x==0 for x in nums) else len(nums)-(reduce(xor, nums, 0)==0)
