11ea7395bb7109718e38b8e634a316f14051311b
bishwa-bandhu-parmar-06 <bishwabandhuparmar06@gmail.com>
2026-09-08 20:36:33 +0530

    3870. Count Commas in Range

---
+++ September-2026/08-09-2026.py
@@ -0,0 +1,12 @@
+
+# 3870. Count Commas in Range
+
+class Solution:
+    def countCommas(self, n: int) -> int:
+        count = 0
+
+        for i in range(1, n + 1):
+            if i >= 1000:
+                count += 1
+
+        return count
