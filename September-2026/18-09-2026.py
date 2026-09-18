9592d639f217f4511520a30d7958c257c23ceceb
bishwa-bandhu-parmar-06 <bishwabandhuparmar06@gmail.com>
2026-09-18 07:27:52 +0530

    1520. Maximum Number of Non-Overlapping Substrings

---
+++ September-2026/18-09-2026.py
@@ -0,0 +1,30 @@
+
+# 1520. Maximum Number of Non-Overlapping Substrings
+
+
+
+class Solution:
+    def maxNumOfSubstrings(self, s: str) -> List[str]:
+        ranges = collections.defaultdict(list)
+        # 1
+        for idx, ch in enumerate(s):
+            ranges[ch].append(idx)
+        # 2
+        for r in ranges:
+            left, right = ranges[r][0], ranges[r][-1]+1
+            templ, tempr = left, right
+            while True:
+                for ch in set(s[templ:tempr]):
+                    templ = min(templ, ranges[ch][0])
+                    tempr = max(tempr, ranges[ch][-1]+1)
+                if (templ, tempr) == (left, right): break
+                left, right = templ, tempr
+            ranges[r] = (templ, tempr)
+        # 3	
+        sorted_ranges = sorted(ranges.values(), key=lambda pair: pair[1])
+        ans, prev = [], 0
+        for start, end in sorted_ranges:
+            if start >= prev:
+                ans.append(s[start:end])
+                prev = end
+        return ans
